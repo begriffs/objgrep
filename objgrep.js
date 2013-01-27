1e871ebfbd76e77e515c57b6343a0ad00eda54bb
Joe Nelson <cred+github@begriffs.com>
2013-01-26 20:41:57 -0600

    Detect cycles and combine array and object cases

--- objgrep.js
+++ objgrep.js
@@ -2,56 +2,55 @@
 /*global toString */
 (function () {
   "use strict";
-  var objgrep = function (root, regex, depth, context) {
-    var className, ret = [], i, newContext;
-    if (typeof depth !== "number") {
-      depth = Infinity;
-    }
-    context = context || '';
-
-    if (depth < 1) {
-      return [];
-    }
-
-    switch (typeof root) {
-    case 'string':
-    case 'number':
-    case 'boolean':
-      if (root.toString().match(regex)) {
-        return [context];
+  var mark = 'visited_by_objgrep',
+    objgrep = function (root, regex, depth, context) {
+      var className, ret = [], i, newContext;
+      if (typeof depth !== "number") {
+        depth = Infinity;
       }
-      break;
-    case 'object':
-      if (root instanceof Array) {
-        for (i = 0; i < root.length; i += 1) {
-          ret = ret.concat(objgrep(
-            root[i],
-            regex,
-            depth - 1,
-            context + '[' + i + ']'
-          ));
+      context = context || '';
+
+      if (depth < 1) {
+        return [];
+      }
+
+      switch (typeof root) {
+      case 'string':
+      case 'number':
+      case 'boolean':
+        if (root.toString().match(regex)) {
+          return [context];
         }
-      } else {
+        break;
+      case 'object':
+        if (!root || root.hasOwnProperty(mark)) {
+          return [];  // cyclic
+        }
+        root[mark] = true;
         for (i in root) {
-          if (i.match(/^[$A-Z_][0-9A-Z_$]*$/i)) {
-            newContext = [context, i].join('.');
-          } else {
-            newContext = context + "['" + i + "']";
+          if (i !== mark) {
+            if (i.match(/^[$A-Z_][0-9A-Z_$]*$/i)) {
+              newContext = [context, i].join('.');
+            } else if (i.match(/^[0-9]+$/)) {
+              newContext = context + "[" + i + "]";
+            } else {
+              newContext = context + "['" + i + "']";
+            }
+            ret = ret.concat(objgrep(
+              root[i],
+              regex,
+              depth - 1,
+              newContext
+            ));
           }
-          ret = ret.concat(objgrep(
-            root[i],
-            regex,
-            depth - 1,
-            newContext
-          ));
         }
+        delete root[mark];
+        break;
+      default:
+        break;
       }
-      break;
-    default:
-      break;
-    }
-    return ret;
-  };
+      return ret;
+    };
 
   Object.prototype.grep = function (regex, depth, context) {
     return objgrep(this, regex, depth, context);
